af5bbaf681c4a1a560f6b6951e99970ea93a4ee7
Galaxycorn <juliann.gaillard@hotmail.fr>
2022-12-17 01:19:28 +0100

    Add dayEleven

--- DayEleven/DayEleven.py
+++ DayEleven/DayEleven.py
@@ -1,7 +1,8 @@
 import re
+import math
 
 def parseMonkeys(f):
-    monkeys = [[] for _ in range(25)]
+    monkeys = [[] for _ in range(15)]
     operation = [[] for _ in range(7)]
     test, targetTrue, targetFalse = [], [], []
     for line in f.readlines():
@@ -24,32 +25,70 @@ def parseMonkeys(f):
                     
     return monkeys, operation, test, targetTrue, targetFalse
             
+def part1(monkeys, operation, test, targetTrue, targetFalse):
+    counter = [[0] for _ in range(8)]
+    for i in range(20):
+        for k in range(8):
+            for j in monkeys[1][k]:
+                j = int(j)
+                counter[k][0] += 1
+                if operation[0][k] == '+':
+                    j += int(operation[1][k])
+                if operation[0][k] == '*':
+                    if operation[1][k] == ' old':
+                        j *= j
+                    else:      
+                        j *= int(operation[1][k])
+                j = j // 3
+                if int(j) % int(test[k]) == 0:
+                    monkeys[1][int(targetTrue[k])].append(j)
+                else: 
+                    monkeys[1][int(targetFalse[k])].append(j)
+                monkeys[1][k] = []
+    return counter
+
+def part2(monkeys, operation, test, targetTrue, targetFalse):
+    counter = [[0] for _ in range(8)]
+    for i in range(10000):
+        for k in range(8):
+            for j in monkeys[1][k]:
+                j = int(j)
+                counter[k][0] += 1
+                if operation[0][k] == '+':
+                    j += int(operation[1][k])
+                if operation[0][k] == '*':
+                    if operation[1][k] == ' old':
+                        j *= j
+                    else:      
+                        j *= int(operation[1][k])
+                j = j % 9699690 #Had to find how to do it because I cound't see it otherwise
+                if int(j) % int(test[k]) == 0:
+                    monkeys[1][int(targetTrue[k])].append(j)
+                else: 
+                    monkeys[1][int(targetFalse[k])].append(j)
+                monkeys[1][k] = []
+    return counter
+
+
 with open("DayEleven/input.txt") as f:
    monkeys, operation, test, targetTrue, targetFalse = parseMonkeys(f)
-   
-for i in range(20):
-    for k in range(7):
-        for j in monkeys[1][k]:
-            if operation[0][k] == '+':
-                j = j + operation[1][k]
-            else: 
-                j = j * operation[1][k]
-            j = j // 3
-            if j % test[k]:
-                monkeys[1][targetTrue[k]].append(monkeys[1][k])
-            else: 
-                monkeys[1][targetFalse[k]].append(monkeys[1][k])
-            monkeys[1][k] = []
-            
-   
-        
-            
-print(monkeys[0][1], monkeys[1][1])
-print(operation[0][1], operation[1][1])
-print(test[1])
-print(targetTrue[1])
-print(targetFalse[1])
 
+counterP1 = part1(monkeys, operation, test, targetTrue, targetFalse)
+max1 = max(counterP1)
+counterP1.remove(max1)
+max2 = max(counterP1)           
+print(max1, max2)
+
+with open("DayEleven/input.txt") as f:
+   monkeys, operation, test, targetTrue, targetFalse = parseMonkeys(f)  
+counterP2 = part2(monkeys, operation, test, targetTrue, targetFalse)
+max3 = max(counterP2)
+counterP2.remove(max3)
+max4 = max(counterP2)           
+print(max3, max4)
+            
+print('Question 1 : ', max1[0]*max2[0]) #61503
+print('Question 2 : ', max3[0]*max4[0]) #14081365540
         
         
             
